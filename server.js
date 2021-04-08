45bb837775b800ec72a8084834751743a39daefa
Matthew Duffield <duffieldml@gmail.com>
2021-04-08 19:10:43 -0400

    Updating server

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
-
 
 // routes
 app.use(require("./routes/api.js"));
